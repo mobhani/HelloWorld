d5f200905c469380702e5c545fc92549201ea910
mobhani <65638996+mobhani@users.noreply.github.com>
2021-08-11 21:01:17 -0400

    removed counter, added killswitch
    App now exits on clicking the button instead of counting clicks.

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,28 +1,29 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 // custom imports
+import android.os.Bundle
 import android.widget.Toast
-import android.widget.TextView
-import android.widget.Button
-
-
-
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // counter
-        var timesClicked = 0
 
-// set on-click listener
         button.setOnClickListener {
-            timesClicked += 1
-            textView.text = timesClicked.toString()
-            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+
+            Toast.makeText(this@MainActivity, "Thank you for clicking me. Goodbye.", Toast.LENGTH_LONG).show()
+            Timer().schedule(4000){finishAffinity()}
+
         }
+
     }
+
+
 }
